037f4e652c1757156650dded9d01107e9384b66f
Jejo <jejomardorongon@gmail.com>
2020-04-15 21:12:00 +0800

    Developed navbar component

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,10 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
+
 const blog = () => {
   return (
     <div>
       <h1>This is our blog page</h1>
-      <Link href="/">Home</Link>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
 
 export default () => (
   <div>
     <h1>Home Page</h1>
     Hello people!
-    <div>
-      <Link to="/blog">Blog Page</Link>
-    </div>
-    <div>
-      <a href="https://www.gatsbyjs.org">Gatsby Page</a>
-    </div>
   </div>
 )
