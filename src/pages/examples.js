c7ed363521b13d5f36f5022544598eb3d4b72390
Jejo <jejomardorongon@gmail.com>
2020-04-16 16:23:03 +0800

    Used Page Query

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -2,16 +2,40 @@ import React from "react"
 import Navbar from "../components/Navbar"
 import HeaderHook from "../examples/Header-Hooks"
 import HeaderStatic from "../examples/Header-Static"
+import { graphql } from "gatsby"
 
-const examples = () => {
+const examples = props => {
+  const { name, age } = props.data.site.siteMetadata.person
+
   return (
     <>
       <Navbar />
       Examples Page
       <HeaderHook />
       <HeaderStatic />
+      <div>
+        <h1>This is a Page Query</h1>
+        <h4>Name: {name}</h4>
+        <h4>Age: {age}</h4>
+      </div>
     </>
   )
 }
+
+export const getData = graphql`
+  query {
+    site {
+      siteMetadata {
+        description
+        title
+        author
+        person: data {
+          age
+          name
+        }
+      }
+    }
+  }
+`
 
 export default examples
