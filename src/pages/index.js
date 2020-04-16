346f79a1cad3da1d958fae9b89ae3efb6707a557
Jejo <jejomardorongon@gmail.com>
2020-04-16 10:49:56 +0800

    Local css using css module

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "../components/layout"
 export default () => (
   <Layout>
     <h1 style={{ color: "red" }}>Home Page</h1>
-    <h2>Lyra Ibong</h2>
+    <h2 className="title">Lyra Ibong</h2>
     Hello people!
   </Layout>
 )
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/layout"
+import styles from "../components/products.module.css"
 
 const products = () => {
   return (
     <Layout>
-      <h1>This is our products page</h1>
+      <h1 className={styles.title}>This is our products page</h1>
+      <h2 className={styles.title}>Wheels bus</h2>
     </Layout>
   )
 }
